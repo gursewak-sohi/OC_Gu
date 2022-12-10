a0bcfb714911d8d748ef418538347fe22f320d6d
gursewak <gursewak@gursewaks-MacBook-Pro.local>
2022-12-10 09:16:52 +0530

    badge

--- js/main.js
+++ js/main.js
@@ -289,7 +289,7 @@
              if (content.length > showChar) {
                  var c = content.substr(0, showChar);
                  var h = content.substr(showChar, content.length - showChar);
-                 var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+                 var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink text-decoration-underline">' + moretext + '</a></span>';
 
                  $(this).html(html);
              }
