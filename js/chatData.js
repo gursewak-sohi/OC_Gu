fa50322b9888994e573df487cc0bd3781a09cd4e
gursewak <gursewak@gursewaks-MacBook-Pro.local>
2023-12-28 16:38:23 +0530

    chat module

--- js/chatData.js
+++ js/chatData.js
@@ -4,67 +4,186 @@
 
 document.addEventListener("alpine:init", () => {
   Alpine.data('chatComponent', () => ({
+      // Users Data
       users: [],
-      isInitialLoading: true,
-      skip: 0,
-      limit: 10,
-      isFetching: false,
-      currentFolder: 'ALL',
-      isError: false,
-      errorMessage: '',
+      isInitialUserLoading: true,
+      usersSkip: 0,
+      usersLimit: 10,
+      isUsersFetching: false,
+      folders : [],
+      currentFolder: '',
+      currentFolderName: 'Loading..',
+      isUserError: false,
+      errorUserMessage: '',
 
-      fetchChatData() {
-          if (this.isFetching) return;
-          
-          this.isFetching = true;
-          this.isError = false;
-          fetch(`https://www.onlinecasting.co.za/apichat/JSON_CASTER_conversations.asp?skip=${this.skip}&limit=${this.limit}&folder=${this.currentFolder}`)
+      // Messages Data
+      messagesData: {},
+      messages : [],
+      isInitialMessageLoading: true,
+      initialMessagesLoaded: false,
+      currentConversationID : 1,
+      messagesSkip: 0,
+      messagesLimit: 10,
+      isMessagesFetching: false,
+      isMessagesError: false,
+      errorMessageMessage: '',
+ 
+
+      isDateDifferentFromPrevious(index) {
+        if (index === 0) return true; // Always show date for the first message in the current batch
+        const currentMessage = this.messages[index];
+        const prevMessage = this.messages[index - 1];
+    
+        // Check if the current message's date is different from the previous message's date
+        return prevMessage.date !== currentMessage.date;
+    },
+      
+      fetchFolders() {
+        fetch(`https://www.onlinecasting.co.za/apichat/JSON_CASTER_conversations_folder.asp`)
+            .then(response => response.json())
+            .then(data => {
+                if (data && Array.isArray(data.folders)) {
+                    this.folders = data.folders;
+                    // Find the folder with default set to True
+                    const defaultFolder = data.folders.find(folder => folder.default === "True");
+                    if (defaultFolder) {
+                        this.currentFolder = defaultFolder.searchname;
+                        this.currentFolderName = defaultFolder.name;
+                    }
+                }
+            })
+      },
+
+      fetchChatUsers() {
+          if (this.isUsersFetching) return;
+          this.isUsersFetching = true;
+          this.isUserError = false;
+          fetch(`https://www.onlinecasting.co.za/apichat/JSON_CASTER_conversations.asp?skip=${this.usersSkip}&limit=${this.usersLimit}&folder=${this.currentFolder}`)
               .then(response => response.json())
               .then(data => {
                   if (data && Array.isArray(data.users)) {
+                    //  console.log(data.users, 'data.users')
                       this.users = [...this.users, ...data.users];
-                      this.skip += this.limit;
+                      this.usersSkip += this.usersLimit;
                   } else {
                       console.log("Data is not an array or is empty");
                   }
               })
               .catch(error => {
                   console.error("Error fetching chat data:", error);
-                  this.isError = true;
-                  this.errorMessage = 'An error occurred while fetching data';  
+                  this.isUserError = true;
+                  this.errorUserMessage = 'An error occurred while fetching data';  
               })
               .finally(() => {
-                  this.isInitialLoading = false;
-                  this.isFetching = false;
+                  this.isInitialUserLoading = false;
+                  this.isUsersFetching = false;
               });
       },
-   
 
-      switchFolder(folder) {
+      switchFolder(folder, folderName) {
           this.currentFolder = folder;
+          this.currentFolderName = folderName;
           this.users = [];
-          this.skip = 0;
-          this.isInitialLoading = true;
-          this.fetchChatData();
+          this.usersSkip = 0;
+          this.isInitialUserLoading = true;
+          this.fetchChatUsers();
       },
 
+      fetchChatMessages() {
+        if (this.isMessagesFetching) return;
+          this.isMessagesFetching = true;
+          const container = document.getElementById('chatMain');
+          const oldScrollHeight = container.scrollHeight;
+          this.isMessagesError = false;
+        fetch(`https://www.onlinecasting.co.za/apichat/JSON_CASTER_conversation.asp?conversationid=${this.currentConversationID}&skip=${this.messagesSkip}&limit=${this.messagesLimit}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data && Array.isArray(data.messages)) {
+                      this.messagesData = data
+                      this.messages = [...this.messages, ...data.messages];
+                      this.messagesSkip += this.messagesLimit;
+                      this.$nextTick(() => {
+                        const newScrollHeight = container.scrollHeight;
+                        container.scrollTop += newScrollHeight - oldScrollHeight;
+                     });
+                     this.$nextTick(() => {
+                        if (!this.initialMessagesLoaded) {
+                            // Scroll to bottom for Inital load
+                            container.scrollTop = container.scrollHeight;
+                            this.initialMessagesLoaded = true;
+                        } else {
+                            // Adjust scroll for older messages
+                            const newScrollHeight = container.scrollHeight;
+                            container.scrollTop += newScrollHeight - oldScrollHeight;
+                        }
+                    });
+                  } else {
+                      console.log("Data is not an array or is empty");
+                }
+            })
+            .catch(error => {
+                console.error("Error fetching chat data:", error);
+                this.isMessagesError = true;
+                this.errorMessageMessage = 'An error occurred while fetching data'; 
+            })
+            .finally(() => {
+                this.isInitialMessageLoading = false;
+                this.isMessagesFetching = false;
+            });
+             
+     },
+
+      setCurrentConversation(conversationid) {
+         this.currentConversationID = conversationid;
+         this.messagesData = {},
+         this.messages = [];
+         this.messagesSkip = 0;
+         this.initialMessagesLoaded = false;
+         this.isInitialMessageLoading = true;
+         this.fetchChatMessages()
+      },
+
+
       init() {
-        let debounceTimer;
+        let debounceTimerForUsers;
+        let debounceTimerForMessages;
         const chatComponent = this; // Store the component context
-    
+
+        // For Chat Sidebar Users Listing
         document.querySelector('#chatSidebar').addEventListener('scroll', function() {
             const threshold = 100; // Pixels from the bottom of the page
             const position = this.scrollTop + this.offsetHeight; // ScrollTop + height of the element
             const height = this.scrollHeight; // Total scrollable height
-    
-            clearTimeout(debounceTimer);
-            debounceTimer = setTimeout(() => {
+
+            clearTimeout(debounceTimerForUsers);
+            debounceTimerForUsers = setTimeout(() => {
                 if (position > height - threshold) {
-                    chatComponent.fetchChatData();
+                    chatComponent.fetchChatUsers();
                 }
             }, 300);
         });
-        this.fetchChatData(); // Initial fetch
+
+        // For Chat Messages Container
+        document.querySelector('#chatMain').addEventListener('scroll', function() {
+            const threshold = 100; // Pixels from the top of the page
+            const position = this.scrollTop; // ScrollTop of the element
+
+            clearTimeout(debounceTimerForMessages);
+            debounceTimerForMessages = setTimeout(() => {
+                if (position < threshold && chatComponent.initialMessagesLoaded) {
+                    chatComponent.fetchChatMessages();
+                }
+            }, 300);
+        });
+
+        
+         // Initial fetch
+        this.fetchFolders();
+        this.fetchChatUsers();
+        this.fetchChatMessages();
     }      
   }));
 });
+
+
+ 
