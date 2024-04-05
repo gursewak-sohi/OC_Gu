a6efc978ef4e4de0d3c308c5bd1490a57a3c0392
gursewak <gursewak@gursewaks-MacBook-Pro.local>
2024-04-05 12:41:50 +0530

    duplicate

--- files/searchData.js
+++ files/searchData.js
@@ -261,12 +261,14 @@ document.addEventListener("alpine:init", () => {
             });
       },
 
-      
+      isListNameExist : false,
       createListSumbit() {
         this.isCreatingList = true;
+        this.isListNameExist = false;
         fetch(`https://www.onlinecasting.dk/api/savedprofiles/create_list_submit.asp?profileid=${this.currentProfileId}&list_name=${this.newListName}`)
             .then(response => response.json())
             .then(data => {
+                // console.log(data, 'data')
                 if (data.Status == 'OK' && data.ShowMessage == 'YES') {
 
                   let starIconClass = data.change_profile_starred === 'ON' ? 'stariconFill' : 'stariconoutline';
@@ -288,21 +290,42 @@ document.addEventListener("alpine:init", () => {
                   if (!statusModalInstance) {
                     statusModalInstance = new bootstrap.Modal(statusModal);
                   }
-                  statusModalInstance.show();  
-                  
+                  this.$nextTick(() => {
+                    setTimeout(() => {
+                      statusModalInstance.show();  
+                    }, 500);
+                  });
+                  this.newListName = '';
                 }
+                else if (data.Status == 'ERROR' && data.listname_exists == 'YES') {
+                  this.isListNameExist = true;
+                  this.statusMessage = data.StatusMessage 
+                } 
                 else if (data.Status == 'ERROR') {
                   this.statusMessageHeadline = data.StatusMessageHeadline
                   this.statusMessage = data.StatusMessage
                   this.messageTextClose = data.text_close
-  
+                  
+                  let createListModal = document.getElementById('createListModal');
+                  let createListModalInstance = bootstrap.Modal.getInstance(createListModal);
+                  if (!createListModalInstance) {
+                    createListModalInstance = new bootstrap.Modal(createListModal);
+                  }
+                  createListModalInstance.hide();
+
                   let statusModal = document.getElementById('statusModal');
                   let statusModalInstance = bootstrap.Modal.getInstance(statusModal);
                   if (!statusModalInstance) {
                     statusModalInstance = new bootstrap.Modal(statusModal);
                   }
-                  statusModalInstance.show();  
+
+                  this.$nextTick(() => {
+                    setTimeout(() => {
+                      statusModalInstance.show();  
+                    }, 500);
+                  });  
                 }  
+                this.newListName = '';
             })
             .catch(error => {
                 console.error("Error bookmarking profile:", error);
