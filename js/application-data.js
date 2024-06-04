836f748ecb05c17b3c221ea66685885c89b3f035
gursewak <sohi.gursewak@gmail.com>
2024-06-04 18:13:37 +0530

    detailed view

--- js/application-data.js
+++ js/application-data.js
@@ -1,35 +1,4 @@
-function reinitializeOwlCarousel() {
-  let sliders = document.querySelectorAll('.latestProfile');
-  
-  sliders.forEach(slider => {
-    // Check if the current element has an Owl Carousel instance
-    if ($.data(slider, 'owl.carousel')) {
-      $(slider).owlCarousel('destroy'); 
-    }
-
-    // Reinitialize the Owl Carousel
-    $(slider).owlCarousel({
-      loop: true,
-      margin: 10,
-      nav: true,
-      responsive: {
-        0: {
-          items: 1
-        },
-        600: {
-          items: 1
-        },
-        1000: {
-          items: 1
-        }
-      }
-    });
-  });
-}
-
-
-
-document.addEventListener("alpine:init", () => {
+ document.addEventListener("alpine:init", () => {
   Alpine.data('applicationComponent', () => ({
     currentView: 'list',
     
